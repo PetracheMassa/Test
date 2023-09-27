4732cf25d9b438dc3d222db1dc7e1063fbe9c884
PetracheMassa <36907424+PetracheMassa@users.noreply.github.com>
2023-09-27 18:22:47 +0000

    Am adaugat un test pentru first.py

--- first.py
+++ first.py
@@ -1,6 +1,5 @@
 #! usr/bin/env python
-import sys
 
 def prima():
     '''Doc pentru prima'''
-    print('Primul nostru git repo')
+    return('Primul repo')
---
+++ test_first.py
@@ -0,0 +1,6 @@
+from first import prima
+
+
+def test_first():
+    '''Test pentru first file'''
+    assert prima() == 'Primul repo'
